c58b496e44dcd677221776bde8934b18c422086a
martin-glimakra <88892064+martin-glimakra@users.noreply.github.com>
2021-11-11 23:31:56 +0100

    1

---
+++ calc.js
@@ -0,0 +1,31 @@
+let tal1
+let tal2
+
+function likamed () {
+    console.log('nu kör likamed funktionen')
+
+    console.log(tal1 + tal2)
+    // console.log(plussar(5 + 3))
+}
+
+function knapp1() {
+    console.log('nu kör knapp1 funktionen')
+    tal1 = 1
+    console.log(tal1)
+}
+
+function knapp2() {
+    console.log('nu kör knapp2 funktionen')
+    tal2 = 2
+    console.log(tal2)
+}
+
+function knapp3() {
+    console.log('nu kör knapp3 funktionen')
+    tal2 = 3
+    console.log(tal2)
+}
+
+// function plussar(param1, param2) {
+//     return param1 + param2 
+// }
